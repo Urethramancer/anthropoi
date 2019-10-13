92f0dc44216fd69b5b4e84f1562757c8e75dce4d
Ronny Bangsund <github@neuralshortcircuit.com>
2019-10-13 18:56:07 +0200

    Implemented password reset.

---
+++ cmd/anthro/user_reset.go
@@ -0,0 +1,47 @@
+package main
+
+import (
+	"errors"
+	"strconv"
+
+	"github.com/Urethramancer/ansi"
+	"github.com/Urethramancer/anthropoi"
+	"github.com/Urethramancer/signor/opt"
+)
+
+// CmdUserReset options.
+type CmdUserReset struct {
+	opt.DefaultHelp
+	User string `placeholder:"USERNAME" help:"Name of user to add." opt:"required"`
+	Len  int    `short:"l" long:"length" help:"Length of password." default:"14"`
+	Cost int    `short:"c" long:"cost" help:"Cost of hashing algorithm. Tweak this to make it at least 500ms to hash on the server." default:"12"`
+}
+
+// Run reset
+func (cmd *CmdUserReset) Run(in []string) error {
+	if cmd.Help || cmd.User == "" {
+		return errors.New(opt.ErrorUsage)
+	}
+
+	db, err := connect()
+	if err != nil {
+		return err
+	}
+
+	defer db.Close()
+	id, err := strconv.ParseInt(cmd.User, 10, 64)
+	var u *anthropoi.User
+	if err != nil {
+		u, err = db.GetUserByName(cmd.User)
+	} else {
+		u, err = db.GetUser(id)
+	}
+	if err != nil {
+		return err
+	}
+
+	pw := anthropoi.GenString(cmd.Len)
+	u.SetPassword(pw, cmd.Cost)
+	m("Changed password for %s%s(%d)%s to %s%s%s", ansi.Blue, u.Usermame, u.ID, ansi.Normal, ansi.Green, pw, ansi.Normal)
+	return nil
+}
