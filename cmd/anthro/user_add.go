52d497b7dbcb55b6378e9ff8e65d1b9f79537160
Ronny Bangsund <github@neuralshortcircuit.com>
2019-10-20 21:06:14 +0200

    Added Dovecot password support.

--- cmd/anthro/user_add.go
+++ cmd/anthro/user_add.go
@@ -11,11 +11,13 @@ import (
 // CmdUserAdd options.
 type CmdUserAdd struct {
 	opt.DefaultHelp
-	Name  string `placeholder:"USERNAME" help:"Name of user to add." opt:"required"`
-	Email string `short:"e" long:"email" help:"Optional e-mail"`
-	First string `short:"f" long:"firstname" help:"Optional first name (the one displayed first - may be family name for some regions)."`
-	Last  string `short:"l" long:"lastname" help:"Optional last name."`
-	Cost  int    `short:"c" long:"cost" help:"Cost of hashing algorithm. Tweak this to around 100ms per hash." default:"11"`
+	Name    string `placeholder:"USERNAME" help:"Name of user to add." opt:"required"`
+	Email   string `short:"e" long:"email" help:"Optional e-mail"`
+	First   string `short:"f" long:"firstname" help:"Optional first name (the one displayed first - may be family name for some regions)."`
+	Last    string `short:"l" long:"lastname" help:"Optional last name."`
+	Cost    int    `short:"c" long:"cost" help:"Cost of hashing algorithm. Tweak this to around 100ms per hash." default:"11"`
+	Dovecot bool   `short:"d" long:"dovecot" help:"Generate a Dovecot-compatible password using SHA512-CRYPT, rather than the default bcrypt hash."`
+	Rounds  int    `short:"r" long:"rounds" help:"Number of rounds to hash SHA512-CRYPT." default:"50000"`
 }
 
 // Run add
@@ -34,6 +36,18 @@ func (cmd *CmdUserAdd) Run(in []string) error {
 	u, err := db.AddUser(cmd.Name, pw, cmd.Email, cmd.First, cmd.Last, "{}", "{}", cmd.Cost)
 	if err != nil {
 		return err
+	}
+
+	if cmd.Dovecot {
+		if cmd.Rounds < 10000 {
+			cmd.Rounds = 10000
+		}
+
+		u.SetDovecotPassword(pw, cmd.Rounds)
+		err = db.SaveUser(u)
+		if err != nil {
+			return err
+		}
 	}
 
 	m("Added user %d with password %s%s%s", u.ID, ansi.Green, pw, ansi.Normal)
