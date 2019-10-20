07d258c0123aab13a11c77e7490d8ffe05e7b7e9
Ronny Bangsund <github@neuralshortcircuit.com>
2019-10-20 14:58:38 +0200

    Refactored.

--- base64.go
+++ base64.go
@@ -1,42 +1,36 @@
 package anthropoi
 
+import (
+	"strings"
+)
+
 const alphabet = "./0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"
 
 // Base6424 used by some password hashing algorithms.
-func Base6424(src []byte) (hash []byte) {
+func Base6424(src string) string {
 	if len(src) == 0 {
-		return []byte{} // TODO: return nil
+		return ""
 	}
 
-	hashSize := (len(src) * 8) / 6
-	if (len(src) % 6) != 0 {
-		hashSize += 1
-	}
-	hash = make([]byte, hashSize)
-
-	dst := hash
+	buf := strings.Builder{}
 	for len(src) > 0 {
 		switch len(src) {
 		default:
-			dst[0] = alphabet[src[0]&0x3f]
-			dst[1] = alphabet[((src[0]>>6)|(src[1]<<2))&0x3f]
-			dst[2] = alphabet[((src[1]>>4)|(src[2]<<4))&0x3f]
-			dst[3] = alphabet[(src[2]>>2)&0x3f]
+			_ = buf.WriteByte(alphabet[src[0]&0x3f])
+			_ = buf.WriteByte(alphabet[((src[0]>>6)|(src[1]<<2))&0x3f])
+			_ = buf.WriteByte(alphabet[((src[1]>>4)|(src[2]<<4))&0x3f])
+			_ = buf.WriteByte(alphabet[(src[2]>>2)&0x3f])
 			src = src[3:]
-			dst = dst[4:]
 		case 2:
-			dst[0] = alphabet[src[0]&0x3f]
-			dst[1] = alphabet[((src[0]>>6)|(src[1]<<2))&0x3f]
-			dst[2] = alphabet[(src[1]>>4)&0x3f]
+			_ = buf.WriteByte(alphabet[src[0]&0x3f])
+			_ = buf.WriteByte(alphabet[((src[0]>>6)|(src[1]<<2))&0x3f])
+			_ = buf.WriteByte(alphabet[(src[1]>>4)&0x3f])
 			src = src[2:]
-			dst = dst[3:]
 		case 1:
-			dst[0] = alphabet[src[0]&0x3f]
-			dst[1] = alphabet[(src[0]>>6)&0x3f]
+			_ = buf.WriteByte(alphabet[src[0]&0x3f])
+			_ = buf.WriteByte(alphabet[(src[0]>>6)&0x3f])
 			src = src[1:]
-			dst = dst[2:]
 		}
 	}
-
-	return
+	return buf.String()
 }
