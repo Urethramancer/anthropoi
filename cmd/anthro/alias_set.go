cabfb123d287d6d27e306a165d7f4d21eb13a707
Ronny Bangsund <github@neuralshortcircuit.com>
2019-10-25 21:09:59 +0200

    Set fully implemented.

--- cmd/anthro/alias_set.go
+++ cmd/anthro/alias_set.go
@@ -9,15 +9,35 @@ import (
 // CmdAliasSet options.
 type CmdAliasSet struct {
 	opt.DefaultHelp
-	Alias  string `placeholder:"" help:""`
-	Target string `placeholder:"" help:""`
+	Alias  string `placeholder:"ALIAS" help:"Alias to create. Must be @ a domain in the database."`
+	Target string `placeholder:"TARGET" help:"Address to forward to. This will be resolved to an actual account if you specify an alias."`
+	JSON   string `short:"j" long:"json" help:"Specify JSON file containing alias:target dictionary pairs of aliases to add."`
 }
 
 // Run set
 func (cmd *CmdAliasSet) Run(in []string) error {
 	if cmd.Help || cmd.Target == "" {
-		return errors.New(opt.ErrorUsage)
+		if cmd.JSON == "" {
+			return errors.New(opt.ErrorUsage)
+		}
 	}
 
+	db, err := connect(name)
+	if err != nil {
+		return err
+	}
+
+	defer db.Close()
+	t, err := db.GetAlias(cmd.Target)
+	if err != nil {
+		return err
+	}
+
+	err = db.SetAlias(cmd.Alias, cmd.Target)
+	if err != nil {
+		return err
+	}
+
+	m("%s -> %s", cmd.Alias, t)
 	return nil
 }
