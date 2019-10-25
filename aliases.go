df754908aca4a557db35055d96f30b2155933a99
Ronny Bangsund <github@neuralshortcircuit.com>
2019-10-25 21:09:37 +0200

    More useful search and a collection struct.

--- aliases.go
+++ aliases.go
@@ -1,7 +1,15 @@
 package anthropoi
 
-// alias character varying(200) COLLATE pg_catalog."default" NOT NULL,
-// target character varying(200) COLLATE pg_catalog."default" NOT NULL,
+// Alias object.
+type Alias struct {
+	Alias  string `json:"alias"`
+	Target string `json:"target"`
+}
+
+// Aliases container.
+type Aliases struct {
+	List []Alias `json:"aliases"`
+}
 
 // SetAlias creates or updates a new alias pointing to an existing target address (which may itself be an alias).
 func (db *DBM) SetAlias(alias, target string) error {
@@ -15,12 +23,34 @@ func (db *DBM) SetAlias(alias, target string) error {
 // GetAlias returns the target for an alias.
 func (db *DBM) GetAlias(alias string) (string, error) {
 	var t string
-	err := db.QueryRow("SELECT value FROM public.variables WHERE key=$1;", alias).Scan(&t)
+	err := db.QueryRow("SELECT target FROM public.aliases WHERE alias=$1;", alias).Scan(&t)
 	if err != nil {
 		return "", err
 	}
 
 	return t, nil
+}
+
+// SearchAliases looks through aliases and their targets containing the match string.
+func (db *DBM) SearchAliases(match string) (*Aliases, error) {
+	rows, err := db.Query("SELECT alias,target FROM public.aliases WHERE alias||target LIKE '%'||$1||'%' ORDER BY target;", match)
+	if err != nil {
+		return nil, err
+	}
+
+	defer rows.Close()
+	var aliases Aliases
+	for rows.Next() {
+		var a Alias
+		err = rows.Scan(&a.Alias, &a.Target)
+		if err != nil {
+			return nil, err
+		}
+
+		aliases.List = append(aliases.List, a)
+	}
+
+	return &aliases, nil
 }
 
 // RemoveAlias deletes an alias.
